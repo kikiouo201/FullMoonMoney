352890761c2b8d469346cb7fcf2b3b1a1af8ff89
kiki.yang <kiki.yang@piapp.com.tw>
2023-07-25 16:23:55 +0800

    Add the dropdownMenu in the monthly page

--- app/src/main/java/com/example/fullmoonmoney/ui/home/Home.kt
+++ app/src/main/java/com/example/fullmoonmoney/ui/home/Home.kt
@@ -1,21 +1,25 @@
 package com.example.fullmoonmoney.ui.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.DateRange
-import androidx.compose.material.icons.outlined.List
-import androidx.compose.material.icons.outlined.ShoppingCart
-import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -23,11 +27,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -42,71 +48,74 @@ fun Home(
     homeViewModel: HomeViewModel = viewModel()
 ) {
     var state by remember { homeViewModel.state }
+    var selectedMonth by remember { mutableStateOf(1) }
+    var expanded by remember { mutableStateOf(false) }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(title = {
-                Text("My App")
-            })
-        },
-        bottomBar = {
-            BottomAppBar(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                homeViewModel.homeCategories.forEachIndexed { index, category ->
-                    NavigationBarItem(selected = state == index,
-                        onClick = { state = index },
-                        icon = {
-                            Icon(
-                                when (category) {
-                                    HomeCategory.General -> Icons.Outlined.List
-                                    HomeCategory.Monthly -> Icons.Outlined.DateRange
-                                    HomeCategory.Assets -> Icons.Outlined.ShoppingCart
-                                    HomeCategory.Project -> Icons.Outlined.Star
-                                }, contentDescription = null
-                            )
-                        },
-                        label =
-                        {
-                            Text(
-                                stringResource(
-                                    when (category) {
-                                        HomeCategory.General -> R.string.general_accounting
-                                        HomeCategory.Monthly -> R.string.monthly_accounting
-                                        HomeCategory.Assets -> R.string.assets
-                                        HomeCategory.Project -> R.string.project
-                                    }
-                                )
-                            )
-                        })
-                }
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text("My App")
+        })
+    }, bottomBar = {
+        BottomAppBar(
+            Modifier.fillMaxWidth()
+        ) {
+            homeViewModel.homeCategories.forEachIndexed { index, category ->
+                NavigationBarItem(selected = state == index, onClick = { state = index }, icon = {
+                    Icon(
+                        imageVector = category.icon,
+                        contentDescription = stringResource(category.categoryName)
+                    )
+                }, label = {
+                    Text(stringResource(category.categoryName))
+                })
             }
         }
-    ) { paddingValues ->
+    }) { paddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
                 .verticalScroll(rememberScrollState())
         ) {
-            Text(
-                modifier = Modifier
-                    .align(Alignment.End)
-                    .padding(20.dp),
-                text = "6月",
-                style = MaterialTheme.typography.bodyLarge
-            )
+            Box(
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                IconButton(modifier = Modifier
+                    .padding(10.dp)
+                    .width(70.dp),
+                    onClick = { expanded = true }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = "$selectedMonth 月")
+                        Icon(
+                            imageVector = Icons.Default.ArrowDropDown,
+                            modifier = Modifier.fillMaxHeight(),
+                            contentDescription = ""
+                        )
+                    }
+                }
+                DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                    repeat(12) {
+                        DropdownMenuItem(text = {
+                            Text(
+                                textAlign = TextAlign.Center,
+                                text = "${it + 1} 月",
+                                modifier = Modifier.fillMaxWidth()
+                            )
+                        }, onClick = {
+                            selectedMonth = it + 1
+                            expanded = false
+                        })
+                    }
+                }
+            }
             Text(
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
                     .height(80.dp)
                     .wrapContentHeight(align = Alignment.CenterVertically),
                 text = "圖表",
-                style = MaterialTheme.typography.bodyLarge
-            )
-            Text(
-                modifier = Modifier.align(Alignment.CenterHorizontally),
-                text = "選擇 tab ${state + 1}",
                 style = MaterialTheme.typography.bodyLarge
             )
             when (homeViewModel.homeCategories[state]) {
@@ -136,9 +145,7 @@ fun Home(
 @Composable
 fun GeneralAccounting(text: String, modifier: Modifier = Modifier) {
     Text(
-        modifier = modifier,
-        text = text,
-        style = MaterialTheme.typography.bodyLarge
+        modifier = modifier, text = text, style = MaterialTheme.typography.bodyLarge
     )
 }
 
--- app/src/main/java/com/example/fullmoonmoney/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/fullmoonmoney/ui/home/HomeViewModel.kt
@@ -1,16 +1,31 @@
 package com.example.fullmoonmoney.ui.home
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.ViewModel
+import com.example.fullmoonmoney.R
 
 class HomeViewModel : ViewModel() {
     var state = mutableStateOf(0)
-    val homeCategories: List<HomeCategory> = listOf(HomeCategory.Monthly, HomeCategory.General, HomeCategory.Assets, HomeCategory.Project)
+    val homeCategories: List<HomeCategory> = listOf(
+        HomeCategory.Monthly,
+        HomeCategory.General,
+        HomeCategory.Assets,
+        HomeCategory.Project
+    )
 }
 
-enum class HomeCategory {
-    Monthly,  // 月記帳
-    General,  // 一般記帳
-    Assets,   // 資產
-    Project,  // 專案
+enum class HomeCategory(
+    val categoryName: Int,
+    val icon: ImageVector
+) {
+    Monthly(R.string.monthly_accounting, Icons.Outlined.DateRange),  // 月記帳
+    General(R.string.general_accounting, Icons.Outlined.List),  // 一般記帳
+    Assets(R.string.assets, Icons.Outlined.ShoppingCart),   // 資產
+    Project(R.string.project, Icons.Outlined.Star)  // 專案
 }
